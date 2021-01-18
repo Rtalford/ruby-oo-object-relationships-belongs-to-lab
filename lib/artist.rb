b59d2029e5cf1b5082557467a905a1391acfec49
Reynelle <Rtalford13@gmail.com>
2021-01-18 15:05:45 -0500

    commit tree d174389b64937dd815cf97a94ec0d884116d9860

---
+++ lib/artist.rb
@@ -0,0 +1,17 @@
+class Artist
+    attr_accessor :name
+end
+
+class Song
+
+    attr_accessor :title, :artist
+end 
+
+class Author
+    attr_accessor :name
+end
+
+class Post 
+
+    attr_accessor :title, :author
+end 
